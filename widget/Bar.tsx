0b67da14c23faddfaf5075d54ab4ffd9a3ebb072
Ingo <me@ingowolf.au>
2025-02-08 16:06:17 +1100

    add workspaces

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,9 +1,33 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk3";
-import { Variable } from "astal";
+import { Variable, bind } from "astal";
 import Hyprland from "gi://AstalHyprland";
 const hyprland = Hyprland.get_default();
 
 const time = Variable("").poll(1000, "date");
+
+function Workspaces() {
+  const hypr = Hyprland.get_default();
+
+  return (
+    <box className="Workspaces">
+      {bind(hypr, "workspaces").as((wss) =>
+        wss
+          .filter((ws) => !(ws.id >= -99 && ws.id <= -2)) // filter out special workspaces
+          .sort((a, b) => a.id - b.id)
+          .map((ws) => (
+            <button
+              className={bind(hypr, "focusedWorkspace").as((fw) =>
+                ws === fw ? "focused" : ""
+              )}
+              onClicked={() => ws.focus()}
+            >
+              {ws.id}
+            </button>
+          ))
+      )}
+    </box>
+  );
+}
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
@@ -17,6 +41,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       application={App}
     >
       <box vertical={true}>
+        <Workspaces />
         <button onClicked="echo hello" halign={Gtk.Align.CENTER}>
           Welcome to AGS!
         </button>
