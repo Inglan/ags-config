4388bd28d951f538fc614c5f93a6a18dcce6a5f2
Ingo <me@ingowolf.au>
2025-02-11 17:00:27 +1100

    add notifications

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk3";
 import style from "./style.scss";
 import Bar from "./widget/Bar";
+import NotificationPopups from "./widget/NotificationPopups";
 
 App.start({
   css: style,
   main() {
     Bar(App.get_monitors()[0]);
+    NotificationPopups(App.get_monitors()[0]);
   },
 });
