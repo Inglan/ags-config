d432177b703dfa271920cb72a1d02b85095fab2a
Ingo <me@ingowolf.au>
2025-02-23 16:23:42 +1100

    add osd from docs

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk3";
 import style from "./style.scss";
 import Bar from "./widget/Bar/Bar";
+import OSD from "./widget/osd/OSD";
 
 App.start({
   css: style,
   main() {
     Bar(App.get_monitors()[0]);
+    OSD(App.get_monitors()[0]);
   },
 });
