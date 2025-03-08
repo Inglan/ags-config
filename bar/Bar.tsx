4006b7ae849481f419fe1abb1df48c31a6d2b042
Ingo <me@ingowolf.au>
2025-03-08 16:00:26 +1100

    add basic media player

--- bar/Bar.tsx
+++ bar/Bar.tsx
@@ -1,9 +1,46 @@
 import { App } from "astal/gtk3";
-import { Astal, Gdk } from "astal/gtk3";
+import { Astal, Gdk, Gtk } from "astal/gtk3";
 import { SysTray } from "./SysTray";
 import { BatteryWidget } from "./BatteryWidget";
 import { Workspaces } from "./Workspaces";
 import { Clock } from "./Clock";
+import { bind } from "astal";
+import Mpris from "gi://AstalMpris";
+
+function Player(player: Mpris.Player) {
+  return (
+    <box>
+      <box
+        className="Cover"
+        css={bind(player, "coverArt").as(
+          (cover) =>
+            `background-image: url('${cover}');min-width: 100px;min-height: 100px; background-size: cover; background-position: center;`,
+        )}
+      />
+      <button
+        onClickRelease={() => {
+          player.play_pause();
+        }}
+      >
+        <box className="icon">
+          {bind(player, "playback_status").as((status) =>
+            status == 1 ? "play_arrow" : "pause",
+          )}
+        </box>
+      </button>
+    </box>
+  );
+}
+
+function Media() {
+  const mpris = Mpris.get_default();
+
+  return (
+    <box className="Media">
+      {bind(mpris, "players").as((ps) => ps.map((player) => Player(player)))}
+    </box>
+  );
+}
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
@@ -20,6 +57,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       <box vertical={true}>
         <Workspaces />
         <box vexpand={true} />
+        <Media />
         <SysTray />
         <BatteryWidget />
         <Clock />
