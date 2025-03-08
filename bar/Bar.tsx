3a50c8fe6791ab5c11e8cf74467dc03238ba9789
Ingo <me@ingowolf.au>
2025-03-08 17:10:21 +1100

    add basic cpu usage

--- bar/Bar.tsx
+++ bar/Bar.tsx
@@ -5,6 +5,30 @@ import { BatteryWidget } from "./BatteryWidget";
 import { Workspaces } from "./Workspaces";
 import { Clock } from "./Clock";
 import { Media } from "./Media";
+import { Variable } from "astal";
+import { bind } from "astal";
+
+let cpuUsage = Variable(0).poll(
+  5000,
+  ["bash", "-c", "top -bn1 | grep Cpu | sed 's/\\,/\\./g' | awk '{print $2}'"],
+  (out: string, prev: number) => parseInt(out),
+);
+
+let memUsage = Variable(0).poll(
+  5000,
+  ["bash", "-c", "free | grep Mem | awk '{print ($3/$2)*100}'"],
+  (out: string, prev: number) => parseInt(out),
+);
+
+function SystemInfo() {
+  return (
+    <box>
+      <label>System Info</label>
+      <label>CPU Usage: {bind(cpuUsage)}%</label>
+      <label>Memory Usage: {bind(memUsage)}%</label>
+    </box>
+  );
+}
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
@@ -22,6 +46,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
         <Workspaces />
         <box vexpand={true} />
         <Media />
+        <SystemInfo />
         <SysTray />
         <BatteryWidget />
         <Clock />
