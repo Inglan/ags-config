f5a14bbc846a2099546917f4e6f72b57ea655df8
Ingo <me@ingowolf.au>
2025-03-08 17:14:18 +1100

    move systeminfo to own file

--- bar/Bar.tsx
+++ bar/Bar.tsx
@@ -1,39 +1,11 @@
 import { App } from "astal/gtk3";
-import { Astal, Gdk, Gtk } from "astal/gtk3";
+import { Astal, Gdk } from "astal/gtk3";
 import { SysTray } from "./SysTray";
 import { BatteryWidget } from "./BatteryWidget";
 import { Workspaces } from "./Workspaces";
 import { Clock } from "./Clock";
 import { Media } from "./Media";
-import { Variable } from "astal";
-import { bind } from "astal";
-
-let cpuUsage = Variable(0).poll(
-  5000,
-  ["bash", "-c", "top -bn1 | grep Cpu | sed 's/\\,/\\./g' | awk '{print $2}'"],
-  (out: string, prev: number) => parseInt(out),
-);
-
-let memUsage = Variable(0).poll(
-  5000,
-  ["bash", "-c", "free | grep Mem | awk '{print ($3/$2)*100}'"],
-  (out: string, prev: number) => parseInt(out),
-);
-
-function SystemInfo() {
-  return (
-    <box vertical={true} className="SystemInfo">
-      <box halign={Gtk.Align.CENTER}>
-        <box className="icon">memory</box>
-        {bind(cpuUsage)}%
-      </box>
-      <box halign={Gtk.Align.CENTER}>
-        <box className="icon">memory_alt</box>
-        {bind(memUsage)}%
-      </box>
-    </box>
-  );
-}
+import { SystemInfo } from "./SystemInfo";
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
