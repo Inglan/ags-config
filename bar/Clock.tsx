cd9f54926a4aafcefb5b0e204ee380ecd19b4685
Ingo <me@ingowolf.au>
2025-03-10 16:58:39 +1100

    add date

--- bar/Clock.tsx
+++ bar/Clock.tsx
@@ -4,18 +4,28 @@ import Gtk from "gi://Gtk?version=3.0";
 let hours = new Variable("");
 let minutes = new Variable("");
 let seconds = new Variable("");
+let dayMonth = new Variable("");
+
 function UpdateTime() {
   const date = new Date();
   hours.set((date.getHours() % 12 || 12).toString().padStart(2, "0"));
   minutes.set(date.getMinutes().toString().padStart(2, "0"));
   seconds.set(date.getSeconds().toString().padStart(2, "0"));
+  dayMonth.set(
+    date.toLocaleString("default", { month: "short", day: "numeric" }),
+  );
 }
 interval(1000, UpdateTime);
 export function Clock() {
   return (
-    <box className="Clock" vertical={true}>
-      <label halign={Gtk.Align.CENTER}>{bind(hours)}</label>
-      <label halign={Gtk.Align.CENTER}>{bind(minutes)}</label>
+    <box hexpand={true} className="Clock" vertical={true}>
+      <label className="ClockTop" halign={Gtk.Align.CENTER}>
+        {bind(hours)}
+      </label>
+      <label className="ClockBottom" halign={Gtk.Align.CENTER}>
+        {bind(minutes)}
+      </label>
+      <label className="Date">{bind(dayMonth)}</label>
     </box>
   );
 }
