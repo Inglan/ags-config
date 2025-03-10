bd82ed1cfab9428eccac73fcaa6f72d5dd452166
Ingo <me@ingowolf.au>
2025-03-10 13:00:38 +1100

    sperate roundedcorners

--- bar/Bar.tsx
+++ bar/Bar.tsx
@@ -10,6 +10,46 @@ import giCairo from "cairo";
 
 import { Audio } from "./Audio";
 
+function RoundedCorners() {
+  return (
+    <box vertical={true} className="Corners">
+      <drawingarea
+        width_request={30}
+        height_request={30}
+        setup={(area) => {
+          area.connect("draw", (area, cr: giCairo.Context) => {
+            cr.arc(30, 30, 30, Math.PI, (3 * Math.PI) / 2);
+            cr.lineTo(0, 0);
+            cr.setSourceRGB(
+              0.11764705882352941,
+              0.11764705882352941,
+              0.1803921568627451,
+            );
+            cr.fill();
+          });
+        }}
+      />
+      <box vexpand={true}></box>
+      <drawingarea
+        width_request={30}
+        height_request={30}
+        setup={(area) => {
+          area.connect("draw", (area, cr: giCairo.Context) => {
+            cr.arc(30, 0, 30, Math.PI / 2, Math.PI);
+            cr.lineTo(0, 30);
+            cr.setSourceRGB(
+              0.11764705882352941,
+              0.11764705882352941,
+              0.1803921568627451,
+            );
+            cr.fill();
+          });
+        }}
+      />
+    </box>
+  );
+}
+
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, BOTTOM } = Astal.WindowAnchor;
 
@@ -32,52 +72,6 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
           <SysTray />
           <BatteryWidget />
           <Clock />
-          {/* <drawingarea
-            width_request={100}
-            height_request={100}
-            setup={(area) => {
-              area.connect("draw", (area, cr: giCairo.Context) => {
-                cr.arc(30, 30, 30, Math.PI, (3 * Math.PI) / 2);
-                cr.lineTo(0, 0);
-                cr.fill();
-              });
-            }}
-          /> */}
-        </box>
-        <box vertical={true} className="Corners">
-          <drawingarea
-            width_request={30}
-            height_request={30}
-            setup={(area) => {
-              area.connect("draw", (area, cr: giCairo.Context) => {
-                cr.arc(30, 30, 30, Math.PI, (3 * Math.PI) / 2);
-                cr.lineTo(0, 0);
-                cr.setSourceRGB(
-                  0.11764705882352941,
-                  0.11764705882352941,
-                  0.1803921568627451,
-                );
-                cr.fill();
-              });
-            }}
-          />
-          <box vexpand={true}></box>
-          <drawingarea
-            width_request={30}
-            height_request={30}
-            setup={(area) => {
-              area.connect("draw", (area, cr: giCairo.Context) => {
-                cr.arc(30, 0, 30, Math.PI / 2, Math.PI);
-                cr.lineTo(0, 30);
-                cr.setSourceRGB(
-                  0.11764705882352941,
-                  0.11764705882352941,
-                  0.1803921568627451,
-                );
-                cr.fill();
-              });
-            }}
-          />
         </box>
       </box>
     </window>
