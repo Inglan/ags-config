379d199e80320e0370fc930ca6b8865976aa6140
Ingo <me@ingowolf.au>
2025-03-10 17:09:15 +1100

    center more

--- bar/Bar.tsx
+++ bar/Bar.tsx
@@ -51,6 +51,7 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
           <Workspaces />
           <box vexpand={true} />
           <Actions />
+          <box vexpand={true} />
           <SystemInfo />
           <BrightnessSlider />
           <Audio />
