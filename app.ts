09264460604740e9c69de9a1d49d18e70d3e0c1e
Ingo <me@ingowolf.au>
2025-03-10 13:19:45 +1100

    remove osd for now

--- app.ts
+++ app.ts
@@ -2,13 +2,11 @@ import { App } from "astal/gtk3";
 import style from "./style.scss";
 import Bar from "./bar/Bar";
 import RoundedCorners from "./roundedcorners/RoundedCorners";
-import OSD from "./osd/OSD";
 
 App.start({
   css: style,
   main() {
     Bar(App.get_monitors()[0]);
     RoundedCorners(App.get_monitors()[0]);
-    OSD(App.get_monitors()[0]);
   },
 });
