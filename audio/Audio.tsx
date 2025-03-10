2f49779b7d520e34bb8fad73023b68ff51366167
Ingo <me@ingowolf.au>
2025-03-10 14:25:01 +1100

    add sinks list

--- audio/Audio.tsx
+++ audio/Audio.tsx
@@ -1,5 +1,10 @@
 import { App } from "astal/gtk3";
 import { Astal, Gdk } from "astal/gtk3";
+import { bind } from "astal";
+import Wp from "gi://AstalWp?version=0.1";
+
+const speaker = Wp.get_default()?.audio.defaultSpeaker!;
+const wp = Wp.get_default()?.audio!;
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { LEFT, BOTTOM } = Astal.WindowAnchor;
@@ -14,6 +19,22 @@ export default function Bar(gdkmonitor: Gdk.Monitor) {
       application={App}
     >
       <box vertical={true}>
+        {bind(wp, "speakers").as((speakers) => {
+          return speakers.map((speaker) => (
+            <button
+              onClick={() => {
+                speaker.set_is_default(true);
+              }}
+              className={bind(speaker, "is_default").as(
+                (isDefault: boolean) => {
+                  return isDefault ? "selected" : "";
+                },
+              )}
+            >
+              {speaker.get_description()}
+            </button>
+          ));
+        })}
         <label className="heading">Audio</label>
         <label className="subheading">Volume</label>
       </box>
